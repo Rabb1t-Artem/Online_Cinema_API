7cc7374b0556c01cae821fe122838ec4b0f38d6d
Mike Shmygel <m.shmygel@gmail.com>
2025-01-31 18:12:53 +0200

    add Carts models, schemas, routs

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -22,6 +22,7 @@ from sqlalchemy.orm import (
 )
 
 from database import Base
+from database.models.carts import CartModel
 from database.validators import accounts as validators
 from security.passwords import hash_password, verify_password
 from security.utils import generate_secure_token
@@ -66,6 +67,12 @@ class UserModel(Base):
 
     group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id", ondelete="CASCADE"), nullable=False)
     group: Mapped["UserGroupModel"] = relationship("UserGroupModel", back_populates="users")
+
+    cart: Mapped["CartModel"] = relationship(
+        "CartModel",
+        back_populates="user",
+        uselist=False
+    )
 
     activation_token: Mapped[Optional["ActivationTokenModel"]] = relationship(
         "ActivationTokenModel",
