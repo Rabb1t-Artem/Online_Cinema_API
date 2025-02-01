695d73fb0c72880c292fd921d3e971fa5fa57c31
94nj111 <94nj111@gmail.com>
2025-02-01 10:54:05 +0200

    add async sessions for db

--- src/database/session_sqlite.py
+++ src/database/session_sqlite.py
@@ -1,37 +1,29 @@
-from contextlib import contextmanager
+from contextlib import asynccontextmanager
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from config import get_settings
 from database import Base
 
 settings = get_settings()
 
-SQLITE_DATABASE_URL = f"sqlite:///{settings.PATH_TO_DB}"
-sqlite_engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
-sqlite_connection = sqlite_engine.connect()
-SqliteSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sqlite_connection)
+SQLITE_DATABASE_URL = f"sqlite+aiosqlite:///{settings.PATH_TO_DB}"
+sqlite_engine = create_async_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
+SqliteSessionLocal = async_sessionmaker(bind=sqlite_engine, autocommit=False, autoflush=False, expire_on_commit=False)
 
 
-def get_sqlite_db() -> Session:
-    db = SqliteSessionLocal()
-    try:
+async def get_sqlite_db() -> AsyncSession:
+    async with SqliteSessionLocal() as db:
         yield db
-    finally:
-        db.close()
 
 
-@contextmanager
-def get_sqlite_db_contextmanager() -> Session:
-    db = SqliteSessionLocal()
-    try:
+@asynccontextmanager
+async def get_sqlite_db_contextmanager() -> AsyncSession:
+    async with SqliteSessionLocal() as db:
         yield db
-    finally:
-        db.close()
 
 
-def reset_sqlite_database():
-    with sqlite_connection.begin():
-        Base.metadata.drop_all(bind=sqlite_connection)
-        Base.metadata.create_all(bind=sqlite_connection)
+async def reset_sqlite_database():
+    async with sqlite_engine.begin() as conn:
+        conn.run_sync(Base.metadata.drop_all)
+        conn.run_sync(Base.metadata.create_all)
