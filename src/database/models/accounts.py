79abf21af2a02cf2a2cb049285539ac7be6162fa
Соня Cавкова <silva.vvss12@gmail.com>
2025-02-01 16:27:11 +0200

    added get_current_user to dependencies

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -18,7 +18,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 
 from database import Base
 from database.models.carts import CartModel
-from database.models.movies import NotificationModel, CommentLikeModel
+from database.models.movies import NotificationModel, CommentLikeModel, MovieLikeModel
 from database.models.orders import OrderModel
 from database.validators import accounts as validators
 from security.passwords import hash_password, verify_password
