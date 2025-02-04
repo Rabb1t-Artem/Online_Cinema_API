457090b4fd71a24a938fa6a743ad73495aa68f96
Serhii <serhijkrasa64uk5@gmail.com>
2025-02-04 14:35:28 +0200

    add tests for crud movie

--- src/main.py
+++ src/main.py
@@ -5,12 +5,10 @@ from routes import accounts_router, profiles_router, movie_router, cart_router
 
 app = FastAPI(title="Movies homework", description="Description of project")
 
-api_version_prefix = "/api/v1"
-
-app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
-app.include_router(profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"])
-app.include_router(movie_router, prefix=f"{api_version_prefix}")
-app.include_router(cart_router, prefix=f"{api_version_prefix}/cart")
+app.include_router(accounts_router, prefix=f"/accounts", tags=["accounts"])
+app.include_router(profiles_router, prefix=f"/profiles", tags=["profiles"])
+app.include_router(movie_router, prefix=f"")
+app.include_router(cart_router, prefix=f"/cart")
 
 
 if __name__ == "__main__":
